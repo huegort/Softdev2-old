c3d0c760edd1219ce8204edcca22076c1c727fef
Andrew Beatty <andrew.beatty@icloud.com>
2016-10-06 14:23:45 +0100

    Update MainClassBJFrame.java

--- SoftDev2_Week04_JFrame/src/ie/gmit/its/MainClassBJFrame.java
+++ SoftDev2_Week04_JFrame/src/ie/gmit/its/MainClassBJFrame.java
@@ -1,7 +1,7 @@
 package ie.gmit.its;
 
 import javax.swing.JFrame;
-
+// keep the main small
 public class MainClassBJFrame {
 	public static void main(String[] args) {
 		JFrame win = new MyJFrame();
